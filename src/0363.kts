31e8a0e6c9dbca196481dbf19d8532c10e19709c
Gogo <gogo81745.moe@gmail.com>
2020-02-02 18:35:25 +0800

    update 0363

--- src/0363.kts
+++ src/0363.kts
@@ -1,27 +1,47 @@
+import java.util.*
 import kotlin.math.max
 
 class Solution {
     fun maxSumSubmatrix(matrix: Array<IntArray>, k: Int): Int {
         val height = matrix.size
         val width = matrix[0].size
-        val arr = Array(height) { IntArray(width) }
+        val partSum = Array(height) { IntArray(width) }
         var max = Int.MIN_VALUE
 
-        for (fromY in matrix.indices) {
-            for (fromX in matrix[fromY].indices) {
-                for (toY in fromY until height) {
-                    for (toX in fromX until width) {
-                        arr[toY][toX] = when {
-                            toX == fromX && toY == fromY -> matrix[toY][toX]
-                            toX == fromX -> arr[toY - 1][toX] + matrix[toY][toX]
-                            toY == fromY -> arr[toY][toX - 1] + matrix[toY][toX]
-                            else -> arr[toY - 1][toX] + arr[toY][toX - 1] - arr[toY - 1][toX - 1] + matrix[toY][toX]
-                        }
-                        if (arr[toY][toX] <= k) max = max(max, arr[toY][toX])
-                    }
+        for (toY in matrix.indices) {
+            for (toX in matrix[0].indices) {
+                partSum[toY][toX] = when {
+                    toX == 0 && toY == 0 -> matrix[toY][toX]
+                    toX == 0 -> partSum[toY - 1][toX] + matrix[toY][toX]
+                    toY == 0 -> partSum[toY][toX - 1] + matrix[toY][toX]
+                    else -> partSum[toY - 1][toX] + partSum[toY][toX - 1] - partSum[toY - 1][toX - 1] + matrix[toY][toX]
                 }
             }
         }
+
+        for (fromY in matrix.indices) {
+            for (toY in fromY until height) {
+                val arr = IntArray(width)
+                for (x in matrix[0].indices) {
+                    arr[x] = partSum[toY][x] - if (fromY == 0) 0 else partSum[fromY - 1][x]
+                }
+                max = max(max, maxSumSubArray(arr, k))
+            }
+        }
         return max
+    }
+
+    private fun maxSumSubArray(arr: IntArray, k: Int): Int {
+        val set = TreeSet<Int>()
+        set.add(0)
+        var result = Int.MIN_VALUE
+        for (it in arr) {
+            val target = set.ceiling(it - k)
+            if (target != null && it - target <= k) {
+                result = max(result, it - target)
+            }
+            set.add(it)
+        }
+        return result
     }
 }
