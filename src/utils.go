d0be88f40f8288a7862b048297fd3d5ef474670c
Gogo <gogo81745.moe@gmail.com>
2025-09-09 22:31:25 +0800

    add lib for ListNode

--- src/utils.go
+++ src/utils.go
@@ -5,3 +5,30 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+
+// Linked List
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func ToList(arr []int) *ListNode {
+	head := &ListNode{}
+	last := head
+	for _, val := range arr {
+		last.Next = &ListNode{
+			Val: val,
+		}
+		last = last.Next
+	}
+	return head.Next
+}
+
+func ToArr(list *ListNode) []int {
+	var res []int
+	for list != nil {
+		res = append(res, list.Val)
+		list = list.Next
+	}
+	return res
+}
